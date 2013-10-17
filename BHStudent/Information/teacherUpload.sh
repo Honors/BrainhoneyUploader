5d7c1baa0f40aff47bb880f84c5390a3fb887989
Matt Neary <neary.matt@gmail.com>
2013-10-17 01:44:05 -0400

    info pages for sub projects and main

--- BHStudent/Information/teacherUpload.sh
+++
@@ -1,7 +0,0 @@
-cookie=`cat $1`
-fname=homescreen.png
-entity=14915878
-curl --form "__VIEWSTATE=/wEPDwUINTIxODU1MzhkZKgW5U0d/kbIvCytxYMA4Y4pbuCZ" \
-     --form "up_file=@$fname" \
-     -H "Cookie=$cookie" \
-     "https://bwhst.brainhoney.com/Editor/AssetHandler.ashx?entityid=$entity&path=Assets/$fname&mediatype=all&.ASPXAUTH=$cookie"
